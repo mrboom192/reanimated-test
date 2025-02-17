c38c28fa5b0145f13d5876706e43c2629c635544
Jason Nguyen <mrboom192@gmail.com>
2025-02-16 21:18:12 -0600

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    presets: [
+      ... // don't add it here :)
+    ],
+    plugins: [
+      ...
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin',
+    ],
+  };
